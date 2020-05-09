8e372f7b364ea3fbeea37f5d9afbc71319a0d399
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-05-09 16:27:09 -0400

    Day 35 - "Deleting items using onDelete()"

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,26 +8,30 @@
 
 import SwiftUI
 
-struct SecondView : View {
-    @Environment(\.presentationMode) var presentationMode
-    var name : String
-    
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     var body: some View {
-        Button("dismiss") {
-            self.presentationMode.wrappedValue.dismiss()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id:\.self) {
+                        Text("\($0)")
+                    }
+                .onDelete(perform: removeRows)
+                }
+                
+                Button("Add number") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                }
+            }
+        .navigationBarItems(leading: EditButton())
         }
     }
-}
-
-struct ContentView: View {
-    @State private var showingSheet = false
-    var body: some View {
-        Button("Show sheet") {
-            self.showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Chris")
-        }
+    
+    func removeRows(at offsets : IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
